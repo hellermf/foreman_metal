500d8280b5171e62d104418d84f8aa12b1bc5bea
Matthew Heller <matthew.f.heller@accre.vanderbilt.edu>
2024-12-09 15:24:36 -0500

    fix missing accessor methods for wrapper val

--- app/models/foreman_metal/metal.rb
+++ app/models/foreman_metal/metal.rb
@@ -2,6 +2,16 @@ module ForemanMetal
   class Metal < ComputeResource
     include MetalConsole
 
+    validates :wrapper, :presence => true
+
+    def wrapper
+      attrs[:wrapper]
+    end
+
+    def wrapper=(value)
+      attrs[:wrapper] = value
+    end
+
     def self.provider_friendly_name
       'Bare metal pseudo provider'
     end
