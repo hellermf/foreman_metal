ff1c8eb7ed3b0c288b5525ff515b896e6956eb38
Matthew Heller <matthew.f.heller@accre.vanderbilt.edu>
2024-12-11 11:07:28 -0600

    very hardcoded and kludgy but vnc console works!

--- app/models/foreman_metal/metal_console.rb
+++ app/models/foreman_metal/metal_console.rb
@@ -1,7 +1,11 @@
 module ForemanMetal
   module MetalConsole
     def console(uuid)
-      #TODO
+      Foreman::Logging.logger('foreman_metal').debug "Starting VNC console..."
+      vm = find_vm_by_uuid(uuid)
+      WsProxy.start(:host => 'cave.vampire', :host_port => 5901, :password => 'REDACTED').merge(
+	      :name => vm.name, :type => 'vnc'
+      )
     end
   end
 end
